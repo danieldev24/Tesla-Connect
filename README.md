2b92db509f5b4fa293f8e7a85793cdd958374df7
HoangSon <sonlth@neoconsulting.com.vn>
2021-11-08 10:01:15 +0700

    init project

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# Tesla-Connect
+# Animated Tesla App Conect using Flutter
+
+## [Watch it on YouTube](https://youtu.be/P629-Z3py1Y)
+
+**Packages we are using:**
+
+- flutter_svg: [link](https://pub.dev/packages/flutter_svg)
+
+This video is for those who want to master Flutter animation. In total there will be four episodes and this is the first one, where you will learn how to create an animated tesla car app using Flutter that works both android and iOS. Users can unlock any door, check battery status also control the air cooler temperature and check the psi of the tires.
+
+In this hole tutorial, we will cover how to use ImplicitlyAnimatedWidge and how to use multiple AnimationController on AnimatedBuilder. How to change curve, also update the AnimatedBuilder with notifyListeners method, and more!
+
+### Animated Tesla App Conect Final UI
+
+![Preview](/gif.gif)
+
+![App UI](/ui.png)
---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,177 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:tesla_animated_app/constanins.dart';
+import 'package:tesla_animated_app/home_controller.dart';
+
+import 'components/battery_status.dart';
+import 'components/door_lock.dart';
+import 'components/tesla_bottom_navigationbar.dart';
+
+class HomeScreen extends StatefulWidget {
+  HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen>
+    with SingleTickerProviderStateMixin {
+  final HomeController _controller = HomeController();
+
+  late AnimationController _batteryAnimationController;
+  late Animation<double> _animationBattery;
+  late Animation<double> _animationBatteryStatus;
+
+  // Thanks for watching Episode 2
+  // On Episode 3 we will show you how you can make this
+
+  void setupBatteryAnimation() {
+    _batteryAnimationController = AnimationController(
+      vsync: this,
+      duration: Duration(milliseconds: 600),
+    );
+
+    _animationBattery = CurvedAnimation(
+      parent: _batteryAnimationController,
+      // Here the animation end on 0.5
+      // it ends on 300 milliseconds
+      curve: Interval(0.0, 0.5),
+    );
+
+    _animationBatteryStatus = CurvedAnimation(
+      parent: _batteryAnimationController,
+      // After a delay we start this animation
+      // after 60 milliseconds delay it start
+      // so it start at 360 and end on 600 milliseconds
+      curve: Interval(0.6, 1),
+    );
+  }
+
+  @override
+  void initState() {
+    setupBatteryAnimation();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _batteryAnimationController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+        animation: Listenable.merge([_controller, _batteryAnimationController]),
+        builder: (context, _) {
+          return Scaffold(
+            bottomNavigationBar: TeslaBottomNavigationBar(
+              onTap: (index) {
+                if (index == 1)
+                  _batteryAnimationController.forward();
+                else if (_controller.selectedBottomTab == 1 && index != 1)
+                  _batteryAnimationController.reverse(from: 0.7);
+                _controller.onBottomNavigationTabChange(index);
+              },
+              selectedTab: _controller.selectedBottomTab,
+            ),
+            body: SafeArea(
+              child: LayoutBuilder(
+                builder: (context, constrains) {
+                  return Stack(
+                    alignment: Alignment.center,
+                    children: [
+                      Padding(
+                        padding: EdgeInsets.symmetric(
+                            vertical: constrains.maxHeight * 0.1),
+                        child: SvgPicture.asset(
+                          "assets/icons/Car.svg",
+                          width: double.infinity,
+                        ),
+                      ),
+                      // Door Locks
+                      AnimatedPositioned(
+                        duration: defaultDuration,
+                        right: _controller.selectedBottomTab == 0
+                            ? constrains.maxWidth * 0.05
+                            : constrains.maxWidth / 2,
+                        child: AnimatedOpacity(
+                          duration: defaultDuration,
+                          opacity: _controller.selectedBottomTab == 0 ? 1 : 0,
+                          child: DoorLock(
+                            isLock: _controller.isRightDoorLock,
+                            press: _controller.updateRightDoorLock,
+                          ),
+                        ),
+                      ),
+                      AnimatedPositioned(
+                        duration: defaultDuration,
+                        left: _controller.selectedBottomTab == 0
+                            ? constrains.maxWidth * 0.05
+                            : constrains.maxWidth / 2,
+                        child: AnimatedOpacity(
+                          duration: defaultDuration,
+                          opacity: _controller.selectedBottomTab == 0 ? 1 : 0,
+                          child: DoorLock(
+                            isLock: _controller.isLeftDoorLock,
+                            press: _controller.updateLeftDoorLock,
+                          ),
+                        ),
+                      ),
+                      AnimatedPositioned(
+                        duration: defaultDuration,
+                        top: _controller.selectedBottomTab == 0
+                            ? constrains.maxHeight * 0.13
+                            : constrains.maxHeight / 2,
+                        child: AnimatedOpacity(
+                          duration: defaultDuration,
+                          opacity: _controller.selectedBottomTab == 0 ? 1 : 0,
+                          child: DoorLock(
+                            isLock: _controller.isBonnetLock,
+                            press: _controller.updateBonnetDoorLock,
+                          ),
+                        ),
+                      ),
+                      AnimatedPositioned(
+                        duration: defaultDuration,
+                        bottom: _controller.selectedBottomTab == 0
+                            ? constrains.maxHeight * 0.17
+                            : constrains.maxHeight / 2,
+                        child: AnimatedOpacity(
+                          duration: defaultDuration,
+                          opacity: _controller.selectedBottomTab == 0 ? 1 : 0,
+                          child: DoorLock(
+                            isLock: _controller.isTrunkLock,
+                            press: _controller.updateTrunkDoorLock,
+                          ),
+                        ),
+                      ),
+
+                      // Battery
+                      Opacity(
+                        opacity: _animationBattery.value,
+                        child: SvgPicture.asset(
+                          "assets/icons/Battery.svg",
+                          width: constrains.maxWidth * 0.45,
+                        ),
+                      ),
+
+                      Positioned(
+                        top: 50 * (1 - _animationBatteryStatus.value),
+                        height: constrains.maxHeight,
+                        width: constrains.maxWidth,
+                        child: Opacity(
+                          opacity: _animationBatteryStatus.value,
+                          child: BatteryStatus(constrains: constrains),
+                        ),
+                      )
+                    ],
+                  );
+                },
+              ),
+            ),
+          );
+        });
+  }
+}
